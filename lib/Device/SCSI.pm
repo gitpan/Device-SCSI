ade31b49da692f947bf153b3e27686048a0d539d
Peter Corlett <abuse@cabal.org.uk>
2011-05-13 18:41:25 +0000

    Import of MOOLI/Device-SCSI-1.004 from CPAN.
    gitpan-cpan-distribution: Device-SCSI
    gitpan-cpan-version:      1.004
    gitpan-cpan-path:         MOOLI/Device-SCSI-1.004.tar.gz
    gitpan-cpan-author:       MOOLI
    gitpan-cpan-maturity:     released

--- lib/Device/SCSI.pm
+++ lib/Device/SCSI.pm
@@ -1,38 +1,80 @@
 package Device::SCSI;
-require 5.005;
+BEGIN {
+  $Device::SCSI::DIST = 'Device-SCSI';
+}
+BEGIN {
+  $Device::SCSI::VERSION = '1.004';
+}
+# ABSTRACT: Perl module to control SCSI devices
+use 5.009004;                   # due to Module::Load
+use warnings;
 use strict;
-use vars qw( $VERSION @ISA );
-# $Id: SCSI.pm,v 1.1 2004/07/15 09:22:22 abuse Exp $
-$VERSION = do { my @r=(q$Revision: 1.1 $=~/\d+/g); sprintf "%d."."%03d"x$#r,@r };
 
 use Carp;
+
+use Module::Load;
 
 # This kludge attempts to load a module with the same name as the OS type.
 # (e.g. SCSI::linux) and then makes this a subclass of that, so we get its
 # methods
 
 BEGIN {
-  eval { require "Device/SCSI/".lc "$^O.pm" };
-  if ($@) {
-    croak "There doesn't seem to be a (working) SCSI driver for \"$^O\":\n$@";
-  }
-  eval "use base 'Device::SCSI::$^O'";
-  croak $@ if $@;
+    my $impl = "Device::SCSI::$^O";
+    load $impl;
+    our @ISA = $impl;
 }
+
+
+sub new {
+    my($pkg, $handle)=@_;
+
+    my $self=bless {}, $pkg;
+    if (defined $handle) {
+        return unless $self->open($handle);
+    }
+    return $self;
+}
+
+sub DESTROY {
+    my $self=shift;
+
+    $self->close();
+}
+
+
+sub inquiry {
+    my $self=shift;
+
+    # FIXME: this ignores the sense data
+    my($data, undef)=$self->execute(pack("C x3 C x1", 0x12, 96), 96); # INQUIRE
+    my %enq;
+    @enq{qw( DEVICE VENDOR PRODUCT REVISION )}=unpack("C x7 A8 A16 A4", $data);
+    return \%enq;
+}
+
+
+1;
+
+__END__
+=pod
 
 =head1 NAME
 
 Device::SCSI - Perl module to control SCSI devices
 
+=head1 VERSION
+
+version 1.004
+
 =head1 SYNOPSIS
 
   use Device::SCSI;
 
-  @devices = Device::SCSI->enumerate;
+  my @devices = Device::SCSI->enumerate;
 
-  $device = Device::SCSI->new($devices[0]);
-  %inquiry = %{ $device->inquiry };
-  ($result, $sense) = $device->execute($command, $wanted, $data);
+  my $device = Device::SCSI->new($devices[0]);
+  my %inquiry = %{ $device->inquiry };
+  my ($result, $sense) = $device->execute($command, $wanted, $data);
   $device->close;
 
 =head1 DESCRIPTION
@@ -43,19 +85,20 @@ object-oriented interface allows for the application to use more than one
 SCSI device simultaneously (although this is more likely to be used by the
 application to cache the devices it needs in a hash.)
 
-As well as the general purpose execute() method, there are also a number
-of other helper methods that can aid in querying the device and debugging.
-Note that the goats and black candles usually required to solve SCSI
-problems will need to be provided by yourself.
+As well as the general purpose execute() method, there are also a number of
+other helper methods that can aid in querying the device and debugging. Note
+that the goats and black candles usually required to solve SCSI problems
+will need to be provided by yourself.
+
+=for test_synopsis my($command, $wanted, $data);
 
 =head1 IMPLEMENTATION
 
-Not surprisingly, SCSI varies sufficiently from OS to OS that each one
-needs to be dealt with separately. This package provides the
-OS-neutral processing. The OS-specific code is provided in a module
-under "Device::SCSI::" that has the same name as $^O does on your
-architecture. The Linux driver is called Device::SCSI::linux, for
-example.
+Not surprisingly, SCSI varies sufficiently from OS to OS that each one needs
+to be dealt with separately. This package provides the OS-neutral
+processing. The OS-specific code is provided in a module under
+"Device::SCSI::" that has the same name as $^O does on your architecture.
+The Linux driver is called Device::SCSI::linux, for example.
 
 The generic class is actually made a subclass of the OS-specific class, not
 the other way round as one might have expected. In other words, it takes the
@@ -63,47 +106,27 @@ opportunity to select its parent after it has started.
 
 =head1 METHODS
 
-=over 4
+=head2 new
 
-=item B<new>
+ $device = Device::SCSI->new;
 
-    $device = Device::SCSI->new;
-
-    $device = Device::SCSI->new($unit_name);
+ $device = Device::SCSI->new($unit_name);
 
 Creates a new SCSI object. If $unit_name is given, it will try to open it.
 On failure, it returns undef, otherwise the object.
 
-=cut
+=head2 enumerate
 
-sub new {
-  my($pkg, $handle)=@_;
-  
-  my $self=bless [], $pkg;
-  if(defined $handle) {
-    return unless $self->open($handle);
-  }
-  return $self;
-}
-
-sub DESTROY {
-  my $self=shift;
-
-  $self->close();
-}
-
-=item B<enumerate>
-
-    @units = Device::SCSI->enumerate;
+ my @units = Device::SCSI->enumerate;
 
 Returns a list of all the unit names that can be given to new() and open().
 There is no guarantee that all these devices will be available (indeed, this
 is unlikely to be the case) and you should iterate over this list, open()ing
 and inquiry()ing devices until you find the one you want.
 
-=item B<open>
+=head2 open
 
-    $device->open($device_name);
+ $device->open($device_name);
 
 Attempts to open a SCSI device, and returns $device if it can, or undef if
 it can't. Reasons for not being able to open a device include it not
@@ -111,20 +134,20 @@ actually existing on your system, or you don't have sufficient permissions
 to use F</dev/sg?> devices. (Many systems require you to be root to use
 these.)
 
-=item B<close>
+=head2 close
 
-    $device->close;
+$device->close;
 
 Closes the SCSI device after use. The device will also be closed if the
 handle goes out of scope.
 
-=item B<execute>
+=head2 execute
 
-  # Reading from the device only
-  ($result, $sense) = $device->execute($command, $wanted);
+ # Reading from the device only
+ my ($result, $sense) = $device->execute($command, $wanted);
 
-  # Writing (and possibly reading) from the device
-  ($result, $sense) = $device->execute($command, $wanted, $data);
+ # Writing (and possibly reading) from the device
+ my ($result, $sense) = $device->execute($command, $wanted, $data);
 
 This method sends a raw SCSI command to the device in question. $command
 should be a 10 or a 12 character string containing the SCSI command. You
@@ -137,46 +160,29 @@ data will appear the array ref $sense. If there is any serious error, for
 example if the device cannot be contacted (and the kernel has not paniced
 from such hardware failure) then an exception may be thrown.
 
-=item B<inquiry>
+=head2 inquiry
 
-    %inquiry = %{ $device->inquiry };
+ %inquiry = %{ $device->inquiry };
 
 This method provides a simple way to query the device via SCSI INQUIRY
 command to identify it. A hash ref will be returned with the following keys:
 
-=over 8
-
-=item DEVICE
+=head3 DEVICE
 
 A number identifying the type of device, for example 1 for a tape drive, or
 5 for a CD-ROM.
 
-=item VENDOR
+=head3 VENDOR
 
 The vendor name, "HP", or "SONY" for example.
 
-=item PRODUCT
+=head3 PRODUCT
 
 The device product name, e.g. "HP35470A", "CD-ROM CDU-8003A".
 
-=item REVISION
+=head3 REVISION
 
 The firmware revision of the device, e.g. "1109" or "1.9a".
-
-=back
-
-=cut
-
-sub inquiry {
-  my $self=shift;
-  
-  my($data, $sense)=$self->execute(pack("C x3 C x5", 0x12, 96), 96); # INQUIRE
-  my %enq;
-  @enq{qw( DEVICE VENDOR PRODUCT REVISION )}=unpack("C x7 A8 A16 A4", $data);
-  return \%enq;
-}
-
-=back
 
 =head1 WARNINGS
 
@@ -205,22 +211,22 @@ There's a Usenet group dedicated to SCSI:
 
   news:comp.periphs.scsi - Discussion of SCSI-based peripheral devices.
 
+=head1 BUGS
+
+This is some of my very early Perl, dating back to 2000, so the code quality
+is not great. It does however work, despite the lack of a test suite to
+prove it.
+
 =head1 AUTHOR
 
-All code and documentation by Peter Corlett <abuse@cabal.org.uk>.
+Peter Corlett <abuse@cabal.org.uk>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2000-2004 Peter Corlett <abuse@cabal.org.uk>. All
-rights reserved.
+This software is copyright (c) 2011 by Peter Corlett.
 
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SUPPORT / WARRANTY
-
-This is free software. IT COMES WITHOUT WARRANTY OF ANY KIND.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
-  
-1;
+
--- lib/Device/SCSI/CDROM.pm
+++ lib/Device/SCSI/CDROM.pm
@@ -1,16 +1,68 @@
 package Device::SCSI::CDROM;
-require 5.005;
-use base Device::SCSI;
+BEGIN {
+  $Device::SCSI::CDROM::DIST = 'Device-SCSI';
+}
+BEGIN {
+  $Device::SCSI::CDROM::VERSION = '1.004';
+}
+# ABSTRACT: Perl module to control SCSI CD-ROM devices
+use 5.005;
+use warnings;
 use strict;
-use vars qw( $VERSION );
-# $Id: CDROM.pm,v 1.1 2004/07/15 09:22:23 abuse Exp $
-$VERSION = do { my @r=(q$Revision: 1.1 $=~/\d+/g); sprintf "%d."."%03d"x$#r,@r };
+use base 'Device::SCSI';
 
 use Carp;
+
+
+sub disc_info {
+    my $self=shift;
+    my($data, $sense)=$self->execute(pack("C x5 C n x", 0x43, 1, 20), 20); # READ TOC
+    return if $sense->[0];
+    my($first, $last)=unpack("x2 C C", $data);
+    return($first, $last);
+}
+
+
+sub toc {
+    my $self=shift;
+
+    my($first,$last)=($self->disc_info);
+    return unless defined $last;
+
+    my %tracks=(
+        FIRST => $first,
+        LAST => $last,
+    );
+    foreach my $track ($first..$last) {
+        my($data, $sense)=$self->execute(pack("C x5 C n x", 0x43, $track, 20), 20); # READ TOC
+        die "Can't read track $track" if $sense->[0];
+        # 2 -> first, 3 -> last 8..11 -> start 16..19 -> end
+        my($start, $end)=unpack("x8 N x4 N", $data);
+        $tracks{$track}={
+            START => $start,
+            FINISH => $end
+        };
+    }
+    $tracks{CD}={
+        START => $tracks{$first}{START},
+        FINISH => $tracks{$last}{FINISH}
+    };
+
+    return \%tracks;
+}
+
+1;
+
+__END__
+=pod
 
 =head1 NAME
 
 Device::SCSI::CDROM - Perl module to control SCSI CD-ROM devices
+
+=head1 VERSION
+
+version 1.004
 
 =head1 SYNOPSIS
 
@@ -23,109 +75,57 @@ This is an incomplete package that may ultimately provide device-specific
 support for CD-ROM and other read-only units. The API is poor and may change
 at any time.
 
-=over 4
+=head1 METHODS
 
-=item B<disc_info>
+=head2 disc_info
 
- my($first, $last)=$device->disc_info;
+ my($first, $last) = $device->disc_info;
 
 This returns the track numbers of the first and last track on the CD
 inserted in the drive.
 
-=cut
-
-sub disc_info {
-  my $self=shift;
-  my($data, $sense)=$self->execute(pack("C x5 C n x", 0x43, 1, 20), 20); # READ TOC
-  return undef if $sense->[0];
-  my($first, $last)=unpack("x2 C C", $data);
-  return($first, $last);
-}    
-
-=item B<toc>
+=head2 toc
 
  my $tracks=$device->toc;
  my $first=$tracks->{FIRST};
  my $last=$tracks->{LAST};
  foreach my $track ($first..$last, 'CD') {
-   my $trackstart=$tracks->{$track}{START};
-   my $trackend=$tracks->{$track}{FINISH};
-   # use these values
+     my $trackstart=$tracks->{$track}{START};
+     my $trackend=$tracks->{$track}{FINISH};
+     # use these values
  }
 
 This reads the Table Of Contents on the CD, and returns a hashref containing
 information on all thr tracks on the CD. The keys are:
 
-=over 8
-
-=item FIRST
+=head3 FIRST
 
 The number of the first track on the CD.
 
-=item LAST
+=head3 LAST
 
 The number of the last track on the CD.
 
-=item CD
+=head3 CD
 
 A hashref with keys B<START> and B<FINISH> mapping to the block numbers of
 the start and end of the CD.
 
-=item (Numbers 1 ... 99)
+=head3 (Numbers 1 ... 99)
 
 A hashref with keys B<START> and B<FINISH> mapping to the block numbers of
 the start and end of the track with the same number as the key.
 
-=back
-
-=cut
-
-sub toc {
-  my $self=shift;
-  
-  my($first,$last)=($self->disc_info);
-  return undef unless defined $last;
-  
-  my %tracks=(
-	      FIRST => $first,
-	      LAST => $last,
-	     );
-  foreach my $track ($first..$last) {
-    my($data, $sense)=$self->execute(pack("C x5 C n x", 0x43, $track, 20), 20); # READ TOC
-    die "Can't read track $track" if $sense->[0];
-    # 2 -> first, 3 -> last 8..11 -> start 16..19 -> end
-    my($start, $end)=unpack("x8 N x4 N", $data);
-    $tracks{$track}={
-		     START => $start,
-		     FINISH => $end
-		    };
-  }
-  $tracks{CD}={
-	       START => $tracks{$first}{START},
-	       FINISH => $tracks{$last}{FINISH}
-	      };
-  
-  return \%tracks;
-}
-
-=back
-
 =head1 AUTHOR
 
-All code and documentation by Peter Corlett <abuse@cabal.org.uk>.
+Peter Corlett <abuse@cabal.org.uk>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2000-2004 Peter Corlett <abuse@cabal.org.uk>. All
-rights reserved.
+This software is copyright (c) 2011 by Peter Corlett.
 
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SUPPORT / WARRANTY
-
-This is free software. IT COMES WITHOUT WARRANTY OF ANY KIND.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
-1;
